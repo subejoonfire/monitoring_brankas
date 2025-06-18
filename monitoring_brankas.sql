b424397538de74d4f533863dfac6870ce7527cd5
subejoonfire <harlanmuradi@gmail.com>
2025-06-18 15:30:52 +0800

    database update

--- monitoring_brankas.sql
+++ monitoring_brankas.sql
@@ -1,53 +1,238 @@
--- Nonaktifkan pengecekan foreign key sementara
-SET FOREIGN_KEY_CHECKS = 0;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 18, 2025 at 07:30 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.3.11
 
--- Table: admin
-DROP TABLE IF EXISTS `admin`;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `monitoring_brankas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
 CREATE TABLE `admin` (
-  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` int UNSIGNED NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Table: data_anggota
-DROP TABLE IF EXISTS `data_anggota`;
-CREATE TABLE `data_anggota` (
-  `id_anggota` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `tag` VARCHAR(20) DEFAULT NULL,
-  `nama_anggota` VARCHAR(255) DEFAULT NULL,
-  PRIMARY KEY (`id_anggota`),
-  UNIQUE KEY `tag` (`tag`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--
+-- Dumping data for table `admin`
+--
 
--- Table: riwayat_berhasil
-DROP TABLE IF EXISTS `riwayat_berhasil`;
-CREATE TABLE `riwayat_berhasil` (
-  `id_riwayat_berhasil` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `id_anggota` INT(11) UNSIGNED DEFAULT NULL,
-  `tanggal_akses` DATE DEFAULT NULL,
-  `waktu_akses` TIME DEFAULT NULL,
-  `foto` TEXT DEFAULT NULL,
-  PRIMARY KEY (`id_riwayat_berhasil`),
-  KEY `id_anggota` (`id_anggota`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `admin` (`id`, `email`, `password`) VALUES
+(1, '11@gmail.com', '$2y$10$PgDPZlPd2SZ0/m/rnHpcvuzh./6e/zN0..SBCZfRl7pgD2CHXZuPS');
 
--- Table: riwayat_gagal
-DROP TABLE IF EXISTS `riwayat_gagal`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `anggota`
+--
+
+CREATE TABLE `anggota` (
+  `id` int UNSIGNED NOT NULL,
+  `tipe` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `nama` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `key` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `anggota`
+--
+
+INSERT INTO `anggota` (`id`, `tipe`, `nama`, `key`) VALUES
+(2, 'RFID', 'Harlan', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` bigint UNSIGNED NOT NULL,
+  `version` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `class` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `group` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `namespace` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `time` int NOT NULL,
+  `batch` int UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `riwayatberhasilkeypad`
+--
+
+CREATE TABLE `riwayatberhasilkeypad` (
+  `id` int UNSIGNED NOT NULL,
+  `id_anggota` int UNSIGNED DEFAULT NULL,
+  `tanggal_akses` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `riwayatberhasilkeypad`
+--
+
+INSERT INTO `riwayatberhasilkeypad` (`id`, `id_anggota`, `tanggal_akses`) VALUES
+(1, 2, '2025-06-18 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `riwayatberhasilrfid`
+--
+
+CREATE TABLE `riwayatberhasilrfid` (
+  `id` int UNSIGNED NOT NULL,
+  `id_anggota` int UNSIGNED NOT NULL,
+  `tanggal_akses` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Dumping data for table `riwayatberhasilrfid`
+--
+
+INSERT INTO `riwayatberhasilrfid` (`id`, `id_anggota`, `tanggal_akses`) VALUES
+(1, 2, '2025-06-18 07:23:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `riwayat_gagal`
+--
+
 CREATE TABLE `riwayat_gagal` (
-  `id_riwayat_gagal` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `tanggal_percobaan` DATE DEFAULT NULL,
-  `waktu_akses` TIME DEFAULT NULL,
-  `tag_tidak_dikenal` VARCHAR(50) DEFAULT NULL,
-  `foto` TEXT DEFAULT NULL,
-  PRIMARY KEY (`id_riwayat_gagal`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` int UNSIGNED NOT NULL,
+  `tanggal_akses` datetime DEFAULT NULL,
+  `key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `foto` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Tambahkan foreign key
-ALTER TABLE `riwayat_berhasil` 
-ADD CONSTRAINT `riwayat_berhasil_ibfk_1` 
-FOREIGN KEY (`id_anggota`) REFERENCES `data_anggota` (`id_anggota`) 
-ON DELETE CASCADE ON UPDATE CASCADE;
+--
+-- Dumping data for table `riwayat_gagal`
+--
 
--- Aktifkan kembali pengecekan foreign key
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `riwayat_gagal` (`id`, `tanggal_akses`, `key`, `foto`) VALUES
+(1, '2025-06-18 15:26:48', 'daadf22', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `riwayatberhasilkeypad`
+--
+ALTER TABLE `riwayatberhasilkeypad`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_anggota` (`id_anggota`);
+
+--
+-- Indexes for table `riwayatberhasilrfid`
+--
+ALTER TABLE `riwayatberhasilrfid`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_anggota` (`id_anggota`);
+
+--
+-- Indexes for table `riwayat_gagal`
+--
+ALTER TABLE `riwayat_gagal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `anggota`
+--
+ALTER TABLE `anggota`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `riwayatberhasilkeypad`
+--
+ALTER TABLE `riwayatberhasilkeypad`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `riwayatberhasilrfid`
+--
+ALTER TABLE `riwayatberhasilrfid`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `riwayat_gagal`
+--
+ALTER TABLE `riwayat_gagal`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `riwayatberhasilkeypad`
+--
+ALTER TABLE `riwayatberhasilkeypad`
+  ADD CONSTRAINT `riwayatberhasilkeypad_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `riwayatberhasilrfid`
+--
+ALTER TABLE `riwayatberhasilrfid`
+  ADD CONSTRAINT `riwayatberhasilrfid_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
